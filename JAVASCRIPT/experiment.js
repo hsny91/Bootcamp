81ba7a5ac110607a6f6e0e5d4d6ecda4437d9761
Husniye <husniyesakmak.91@gmail.com>
2021-03-19 20:40:37 +0100

    experiment.js was update

--- JAVASCRIPT/experiment.js
+++ JAVASCRIPT/experiment.js
@@ -1,3 +1,11 @@
-const monthlaRent=500;
-const yearlyRent=monthlaRent*12;
-console.log(yearlyRent);
+const monthlyRent=500;
+
+let yearlyRent=monthlyRent*12;
+
+console.log(yearlyRent);// cikti 6000 olur.
+
+console.log(yearlyRent,monthlyRent);// cikti 6000 500
+
+yearlyRent=1;
+
+console.log(yearlyRent);//cikti ilk satir 6000 ikinci satir 1 olur 
