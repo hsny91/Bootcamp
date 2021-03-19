fc8f33db3d63ec43dfaf31d9de2f0fbe4d131493
Husniye <husniyesakmak.91@gmail.com>
2021-03-19 21:06:45 +0100

    experiment.js was update

--- JAVASCRIPT/experiment.js
+++ JAVASCRIPT/experiment.js
@@ -9,3 +9,21 @@ console.log(yearlyRent,monthlyRent);// cikti 6000 500
 yearlyRent=1;
 
 console.log(yearlyRent);//cikti ilk satir 6000 ikinci satir 1 olur 
+const firstName= "Brian"
+const lastName="Holt"
+const sentence=`Hello ${firstName} ${lastName} ! How are you ?!`
+console.log(sentence)//String
+
+const num=5.00001;//number
+console.log(num+1.123);
+
+const isTheSkyBlue=true;//boolean
+console.log(isTheSkyBlue)//true
+
+ //if statements
+if(isTheSkyBlue){
+    console.log("The sky is Blue")
+} else {
+    console.log("The sky is not blue?")
+}
+
