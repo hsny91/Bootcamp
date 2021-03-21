a455362aa25292c0f70661dcbafeb47725cce115
Husniye <husniyesakmak.91@gmail.com>
2021-03-21 17:21:43 +0100

    app.js was update

--- JAVASCRIPT/DOM/app.js
+++ JAVASCRIPT/DOM/app.js
@@ -12,3 +12,10 @@ const button = document.querySelector('.event-button');
 button.addEventListener('click', function () {
   alert("Hey there!");
 });
+
+const input = document.querySelector('.input-to-copy');
+  const paragraph = document.querySelector('.p-to-copy-to');
+
+  input.addEventListener("keyup", function() {
+    paragraph.innerText  = input.value;
+  })
