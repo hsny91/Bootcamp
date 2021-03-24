3a0d053151da40f459e76320ba01db74b97af619
Husniye <husniyesakmak.91@gmail.com>
2021-03-24 14:06:18 +0100

    project was updated

--- AJAX/doggos.js
+++ AJAX/doggos.js
@@ -1,14 +1,37 @@
 const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
 
-const promise = fetch(DOG_URL);
+// const promise = fetch(DOG_URL);
 
-promise
-  .then(function(response) {
-    const processingPromise = response.json();
-    return processingPromise;
-  })
-  .then(function(processedResponse) {
-    console.log(breeds);
-  });
+// promise
+//   .then(function(response) {
+//     const processingPromise = response.json();
+//     return processingPromise;
+//   })
+//   .then(function(processedResponse) {
+//     console.log(breeds);
+//   });
 
-console.log("this will log first");
+// console.log("this will log first");
+
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+
+
+const doggos = document.querySelector(".doggos");
+
+function addNewDoggo() {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function(response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function(processedResponse) {
+      const img = document.createElement("img");
+      img.src = processedResponse.message;
+      img.alt = "Cute doggo";
+      doggos.appendChild(img);
+    });
+}
+
+document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
